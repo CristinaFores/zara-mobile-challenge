83334e2352362970e38696aa0c2b1a16919b9020
Cristina Fores Campos <cristinaforescampos1992@gmail.com>
2026-06-19 13:43:04 +0200

    chore: set up Jest and Testing Library

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+/** @type {import('jest').Config} */
+const config = {
+  roots: ['<rootDir>/src'],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  coverageProvider: 'v8',
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/app/layout.tsx',
+    '!src/app/page.tsx',
+    '!src/types/**',
+  ],
+}
+
+module.exports = createJestConfig(config)
